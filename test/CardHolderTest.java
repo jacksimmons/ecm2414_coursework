1b2a059b44c431911517db71514f473af9c5222e
RossScottSanders <115005108+RossScottSanders@users.noreply.github.com>
2022-11-22 16:19:57 +0000

    CardholderTest

--- test/CardHolderTest.java
+++ test/CardHolderTest.java
@@ -1,20 +1,33 @@
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
 public class CardHolderTest {
     CardHolder cardHolder = new CardHolder();
+    CardDeck leftDeck = new CardDeck(1);
+    CardDeck rightDeck = new CardDeck(2);
+    Player testPlayer = new Player(1,leftDeck,rightDeck);
 
     @Test
     public void addCard() {
+        Card card = new Card(1);
+        cardHolder.addCard(card);
+        assert cardHolder.getHandValues().size() == 1;
+        assert cardHolder.hand.contains(card);
     }
 
     @Test
     public void getHandValues() {
-    }
+        /*
+        Tests with empty hand
+         */
+        cardHolder.hand = new ArrayList<>();
+        assert cardHolder.getHandValues().size() == 0;
 
-    @Test
-    public void getStringHandValues() {
+        cardHolder.addCard(new Card(0));
+        assert cardHolder.getHandValues().get(0) == 0;
     }
 
     @Test
