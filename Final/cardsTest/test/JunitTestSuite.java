2e04998f4a8ab94c021aef2b9cb9a57839ae3d0b
piklman <56202789+piklman@users.noreply.github.com>
2022-11-23 17:39:26 +0000

    Revert "finalisation"
    This reverts commit fffb51bd6da7be5565cb67ee519dfeca321ae60d.

---
+++ Final/cardsTest/test/JunitTestSuite.java
@@ -0,0 +1,14 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+        TestPlayer.class,
+        TestCardDeck.class,
+        TestCardHolder.class
+})
+
+public class JunitTestSuite {
+}
